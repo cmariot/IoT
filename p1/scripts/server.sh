0e740df15d332ec1e047ee56d399494de7fd3b3a
cmariot <contact@charles-mariot.fr>
2024-05-27 10:25:28 +0200

    p1: a tester a 42

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-# Set the timezone to Europe/Paris
-sudo timedatectl set-timezone Europe/Paris
+# # Install k3s on the server
+curl -sfL https://get.k3s.io | sh -s - server --write-kubeconfig-mode 644 --node-ip=192.168.56.110
 
-# Add the k3s server worker to the /etc/hosts file
-sudo echo "192.168.56.111 cmariotSW" >> /etc/hosts
-
-# Install k3s on the server
-# curl -sfL https://get.k3s.io | sh -s - server --flannel-iface=eth1 --write-kubeconfig-mode 644
-
-# Share the k3s server-token with the host machine
-# mkdir -p /vagrant/confs
-# sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/confs/token
+# # Share the k3s server-token with the host machine
+mkdir -p /vagrant/confs
+sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/confs/token
--- p1/scripts/server_worker.sh
+++ p1/scripts/server_worker.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Set the timezone to Europe/Paris
-sudo timedatectl set-timezone Europe/Paris
-
-# Add the k3s server to the /etc/hosts file
-sudo echo "192.168.56.110 wloS" >> /etc/hosts
-
 # Install k3s on the server worker
-# curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$(cat /vagrant/confs/token) sh -s - agent --flannel-iface=eth1
+curl -sfL https://get.k3s.io | sh -s - agent --server https://192.168.56.110:6443 --token=$(cat /vagrant/confs/token) --node-ip=192.168.56.111
+
