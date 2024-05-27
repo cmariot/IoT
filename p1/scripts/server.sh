22cd67243bd39d21b22da63f13ce74b6e216878a
Charles <contact@charles-mariot.fr>
2024-05-27 14:21:46 +0200

    TODO: merge for eth1 nternal ip test

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-# # Install k3s on the server
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode=644 --node-ip=192.168.56.110
+# Install k3s on the server
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode=644
 
-# # Share the k3s server-token with the host machine
+
+# Share the k3s server-token with the host machine
 mkdir -p /vagrant/confs
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/confs/token
