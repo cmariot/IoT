8055e2a29d6588d4ea6726db97cf5d3a8aae6c96
Wen <60364538+iamwen1023@users.noreply.github.com>
2024-05-27 14:19:35 +0200

    Update server.sh

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # # Install k3s on the server
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode=644 --node-ip=192.168.56.110
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode=644 --node-external-ip="192.168.56.110" --flannel-iface="eth1"
 
 # # Share the k3s server-token with the host machine
 mkdir -p /vagrant/confs
