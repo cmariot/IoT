88e2c0a7e3b89e975b5c857175735b00166f6c70
Charles <contact@charles-mariot.fr>
2024-05-28 15:45:15 +0200

    apps pushed on docker hub

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -3,4 +3,5 @@
 # Install k3s on the server
 curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode=644 --flannel-iface="eth1"
 
-# kubectl apply -f ../confs/deplyment.yaml
+kubectl apply -f /vagrant/confs/deployment.yaml
+kubectl apply -f /vagrant/confs/service.yaml
