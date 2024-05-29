caf2d9d22dc3c118f9b83e3197bb63da5b521ef4
iamwen1023 <ursula771023@gmail.com>
2024-05-29 16:52:35 +0200

    setup.sh

---
+++ p3/scripts/setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#docker
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install k3d 
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+# Verify Docker installation
+sudo systemctl status docker
+
+# Verify k3d installation
+k3d version
+
+# Install kubectl x86_64
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+# Verify kubectl installation
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+
+echo "Docker, k3d, and kubectl installation complete!"
